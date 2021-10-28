67c628c124d1638a1f44cffcb8c61c087553a1f7
Alberto Helbig <albertohelbig@gmail.com>
2021-10-28 10:45:35 -0300

    feat: add header component

---
+++ src/Components/HeaderPage/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './PageTitle.css';
+
+const PageTitle = () => {
+
+    return (
+        <header>
+            <div className="header-text">
+                <h3 className="header-text-1">Forms</h3>
+                <h2 className="header-text-2">First Tab</h2>
+            </div>
+        </header>
+    );
+}
+
+export default PageTitle;
