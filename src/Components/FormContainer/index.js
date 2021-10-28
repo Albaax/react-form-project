f89281f3f40a82daebc22a99521ba4e118535c5d
Alberto Helbig <albertohelbig@gmail.com>
2021-10-28 10:44:16 -0300

    feat: add structure of form container

---
+++ src/Components/FormContainer/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './FormContainer.css';
+
+const FormContainer = () => {
+
+    return (
+        <div className="form-container">
+            <h2>teste</h2>
+        </div>
+    );
+}
+
+export default FormContainer;
