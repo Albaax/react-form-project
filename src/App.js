883d266e43231ac8b78268631da88fff4b0ee045
Alberto Helbig <albertohelbig@gmail.com>
2021-10-28 10:51:51 -0300

    feat: add default structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import React from 'react';
+import HeaderPage from './Components/HeaderPage';
+import FormContainer from './Components/FormContainer';
 
-function App() {
-  return (
-    <div></div>
-  );
+const App = () => {
+
+    return (
+        <main>
+            <HeaderPage/>
+            <section>
+                <FormContainer/>
+            </section>
+        </main>
+    );
 }
 
 export default App;
