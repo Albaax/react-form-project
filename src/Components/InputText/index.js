dc3f54b2c90e9183527243926d6f2b97dd1ec245
Alberto Helbig <albertohelbig@gmail.com>
2021-11-03 19:41:46 -0300

    feat: add controlled inputs

--- src/Components/InputText/index.js
+++ src/Components/InputText/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import './InputText.css';
 
-const InputText = ({inputName, inputLabel, inputType, placeholder}) => {
-    
+const InputText = ({inputName, inputLabel, inputType, placeholder, changeData}) => {
+    const getData = event => {
+        changeData(event.target.value);
+    }
+
     return (
         <label className="labels" htmlFor={inputName}>
            {inputLabel}
-           <input type={inputType} id={inputName}
-             className="inputs" placeholder={placeholder} />
+           <input 
+           onChange={getData}
+           type={inputType} id={inputName}
+           className="inputs" placeholder={placeholder} />
         </label> 
     );
 }
