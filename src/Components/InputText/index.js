c774185cd7ea4d7bd885a5f45c83bc5a98038cb9
Alberto Helbig <albertohelbig@gmail.com>
2021-11-04 21:14:53 -0300

    feat: add state value and props required

--- src/Components/InputText/index.js
+++ src/Components/InputText/index.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import './InputText.css';
 
-const InputText = ({inputName, inputLabel, inputType, placeholder, changeData}) => {
+const InputText = ({inputName, inputLabel, 
+                    inputType, placeholder, 
+                    changeData, inputValue, 
+                    ...props}) => {
     const getData = event => {
         changeData(event.target.value);
     }
-
+    
     return (
         <label className="labels" htmlFor={inputName}>
            {inputLabel}
-           <input 
+           <input value={inputValue}
            onChange={getData}
            type={inputType} id={inputName}
-           className="inputs" placeholder={placeholder} />
+           className="inputs" placeholder={placeholder}
+           {...props} />
         </label> 
     );
 }
